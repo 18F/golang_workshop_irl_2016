aff2731a4391b039127b01577490cb43cbc6c38e
James C Scott III <jcscottiii@users.noreply.github.com>
2016-05-04 14:21:35 -0400

    Update goprojectup.sh

--- scripts/goprojectup.sh
+++ scripts/goprojectup.sh
@@ -7,6 +7,7 @@ if ! [ -x "$(command -v $GOPATH/bin/dlv)" ]; then
 	DOWLOAD_FOLDER=delve
 	wget -O $ZIP_FILE https://bintray.com/jetbrains/golang/download_file?file_path=com%2Fjetbrains%2Fdelve%2F0.10.231%2Fdelve-0.10.231.zip
 	unzip -o $ZIP_FILE -d $DOWLOAD_FOLDER
+	mkdir -p $GOPATH/bin/
 	cp $DOWLOAD_FOLDER/dlv/mac/dlv $GOPATH/bin/
 	rm $ZIP_FILE
 	rm -rf $DOWLOAD_FOLDER
